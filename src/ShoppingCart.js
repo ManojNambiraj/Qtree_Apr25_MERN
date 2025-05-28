3b5a68ca8da08d57dbfe940ee81791bfc784301a
ManojNambiraj <postboxmano@gmail.com>
2025-05-28 20:50:46 +0530

    shopping cart addtocart

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,100 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShoppingCart.css";
+import img1 from "./assets/sony.png"
+import img2 from "./assets/apple.jpg"
+import img3 from "./assets/samsung.png"
+import ProductCard from "./components/ProductCard";
 
 function ShoppingCart() {
+
+  const datas = [
+    {
+      id: 1,
+      product_name: "Sony",
+      price: 20000,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img1,
+    },
+    {
+      id: 2,
+      product_name: "Apple",
+      price: 150000,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img2,
+    },
+    {
+      id: 3,
+      product_name: "Samsung",
+      price: 45000,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img3,
+    },
+    {
+      id: 4,
+      product_name: "Xiomi",
+      price: 12000,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img1,
+    },
+    {
+      id: 5,
+      product_name: "Oppo",
+      price: 25000,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img2,
+    },
+    {
+      id: 6,
+      product_name: "Vivo",
+      price: 30000,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img3,
+    },
+  ];
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCartItems([...cartItems, product])
+  }
+
+  console.log(cartItems);
+  
+
   return (
     <div className="container-fluid mymain">
       <div className="row">
         <div className="left_section col-10">
           <div className="row">
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://g.sdlcdn.com/imgs/k/x/large/HomePro-Automatic-Power-2000-Watt-SDL268981999-1-67c49.webp?w=220&h=258&sharp=7"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://g.sdlcdn.com/imgs/k/x/large/HomePro-Automatic-Power-2000-Watt-SDL268981999-1-67c49.webp?w=220&h=258&sharp=7"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://g.sdlcdn.com/imgs/k/x/large/HomePro-Automatic-Power-2000-Watt-SDL268981999-1-67c49.webp?w=220&h=258&sharp=7"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://g.sdlcdn.com/imgs/k/x/large/HomePro-Automatic-Power-2000-Watt-SDL268981999-1-67c49.webp?w=220&h=258&sharp=7"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://g.sdlcdn.com/imgs/k/x/large/HomePro-Automatic-Power-2000-Watt-SDL268981999-1-67c49.webp?w=220&h=258&sharp=7"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
+            {datas.map((item) => {
+              return (
+                <ProductCard item={item} handleAddToCart={handleAddToCart} />
+              );
+            })}
           </div>
         </div>
-        <div className="right_section col-2"></div>
+        <div className="right_section col-2">
+          <ol class="list-group list-group-numbered">
+            {
+              cartItems.map((citem) => {
+                return (
+                  <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                      <div class="fw-bold">{citem.product_name}</div>
+                      {citem.price}
+                    </div>
+                    <span class="badge text-bg-primary rounded-pill">X</span>
+                  </li>
+                );
+              })
+            }
+            
+            
+          </ol>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ProductCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ProductCard(props) {
+  return (
+    <div class="card" style={{ width: "18rem" }}>
+      <img src={props.item.img} class="card-img-top" alt="..." height={250} />
+      <div class="card-body">
+        <h5 class="card-title">{props.item.product_name}</h5>
+        <h5 class="card-text">{props.item.price}</h5>
+        <p class="card-text">{props.item.desc}</p>
+        <button class="btn btn-primary" onClick={() => {props.handleAddToCart(props.item);}}>
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
